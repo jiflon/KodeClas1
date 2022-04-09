6f4e779e45d00a4d4955dc7b40d1cff8c16f435d
Jonathan Flores <85594501+jiflon@users.noreply.github.com>
2022-04-09 11:22:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+//practica con ale
+//tipo de datos 04/04/22
+
+/*function first( name, num ){
+    console.log("tipo de dato", typeof name)
+    console.log("tipo de dato", typeof num)
+}
+
+ first ("juan", 20)*/
+ 
+ //---------------Clase 1 IVAN ---- 12/0422--------------------------------------------------
+ 
+ alert("hola");
+
+ //array
+
+ let frutas = ["manzana", "sandia", "pera"];
+
+ let fruta = {
+     nombre: "manzana",
+     color: "verde"
+ }
+
+ fruta.color
+
+ console.log(frutas[0]);
+
+ console.log( frutas.length);
+
+ function sumar (num1, num2){
+     return num1 + num2;
+ };
+/*
+ sumar(1,5);
+ console.log(sumar(1,5));
+*/
+
+let resultado = sumar(1,5);
+console.log(resultado);
+
+//scope
+//variable global al inicio del codigo
+let nombre = "nestor";
+
+function persona(){
+    console.log(nombre);
+};
+
+function persona2(){
+    console.log(nombre);
+};
+
+
+//operadores
+
+//`` alt96 template literal
+//
+
+
+
+
+
+
